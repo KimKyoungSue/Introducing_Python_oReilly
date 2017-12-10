1569edc461a14d4a2c08d15eeb96f7d63e2d1d07
KimKyoungsue <0531sylee@naver.com>
2017-12-10 20:32:54 +0900

    Repeat with while
    Repeat with while(4.4)

--- Chapter4_Py Crust: Code Structures.py
+++ Chapter4_Py Crust: Code Structures.py
@@ -1,6 +1,7 @@
 #This is the Py Crust: Code Structures
 
-#if, elif, else
+#Compare with if, elif, and else
+"""
 disaster = True
 if disaster:
     print("Woe!")
@@ -35,3 +36,36 @@ if some_list:
     print("There's something in here")
 else:
     print("Hey!, It's empty")
+"""
+
+#Repeat with while
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
+    
+while True:
+    stuff = input("String to capitalize [type q to quit]: ")
+    if stuff == "q":
+        break
+    print(stuff.capitalize())
+
+while True:
+    value = input("Integer, please [q to quit]: ")
+    if value == "q":
+        break
+    number = int(value)
+    if number %2 == 0:
+        continue
+    print(number, "squared is", number*number)
+
+numbers = [1,3,4,5]
+position = 0
+while position < len(numbers):
+    number = numbers[position]
+    if number % 2 == 0:
+        print('Found even number', number)
+        break
+    position += 1
+else:
+    print('No even number found')
