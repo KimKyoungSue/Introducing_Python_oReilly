12baf38a96dbba4a5da89b03c296290f158509f4
KimKyoungsue <0531sylee@naver.com>
2017-12-10 18:34:43 +0900

    Compare with if, elif, and else
    Compare with if, elif, and else (4.3)

--- Chapter4_Py Crust: Code Structures.py
+++ Chapter4_Py Crust: Code Structures.py
@@ -1,2 +1,37 @@
 #This is the Py Crust: Code Structures
-print("Hellowolrd")
+
+#if, elif, else
+disaster = True
+if disaster:
+    print("Woe!")
+else:
+    print("Whee!")
+
+furry = True
+small = True
+if furry:
+    if small:
+        print("It's a cat.")
+    else:
+        print("It's a bear")
+else:
+    if small:
+        print("It's a skink!")
+    else:
+        print("It's a human. Or a hairless bear.")
+
+color = "puce"
+if color == "red":
+    print("It's a tomato")
+elif color == "green":
+    print("It's a green pepper")
+elif color == "bee purple":
+    print("I don't know what it is, but olny bees can see it")
+else:
+    print("I've never heard of the color", color)
+
+some_list = []
+if some_list:
+    print("There's something in here")
+else:
+    print("Hey!, It's empty")
